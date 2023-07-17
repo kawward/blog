2e00c212bd1377215a734e1be323db357ee43f8d
Mohamed Bella <63130176+woxbott@users.noreply.github.com>
2023-07-17 22:48:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app')
 //Set up default mongoose connection
-var mongoDB = 'mongodb+srv://mohamedbella:mohamedbella@blog.l8fcx9z.mongodb.net/?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://mohamedbella:mohamedbella@blog.l8fcx9z.mongodb.net/blog';
 mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology: true })
 .then(() => {
      console.log('DATAbase connected successfuly')
